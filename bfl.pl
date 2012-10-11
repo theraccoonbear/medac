1184b0d906d1d0d63874080829ab6dbc6e1311d3
Don Smith <don.c.smith@gmail.com>
2012-10-10 19:32:51 -0500

    initial checkin

---
+++ bfl.pl
@@ -0,0 +1,440 @@
+#!/usr/bin/perl
+use JSON::XS;
+use strict;
+use warnings;
+use Data::Dumper;
+use File::Basename;
+use Video::FrameGrab;
+use POSIX;
+use MIME::Base64;
+use GD;
+use Image::Resize;
+use Digest::MD5 qw(md5 md5_hex);
+use Config::Auto;
+use WebService::TVRage::EpisodeListRequest;
+use WebService::TVRage::ShowSearchRequest;
+use Text::Levenshtein qw(distance);
+
+$| = 0;
+
+my $config = Config::Auto::parse();
+
+my $shsrch = WebService::TVRage::ShowSearchRequest->new();
+my $epsrch = WebService::TVRage::EpisodeListRequest->new();
+
+my $tvr_cache;
+
+
+sub logMsg {
+	my $msg = shift @_ || '...';
+	print "$msg\n";
+}
+
+sub bestMatch {
+	my $orig = shift @_;
+	my @vals = @{shift @_};
+	my $best_dist = 100000;
+	my $best_match = '';
+	
+	foreach my $v (@vals) {
+		my $d = distance($orig, $v);
+		
+		if ($d == 0) { return $v; }
+		
+		if ($d < $best_dist) {
+			$d = $best_dist;
+			$best_match = $v;
+		}
+	}
+	
+	return $best_match;
+}
+sub tvrEpisodeSearch {
+	my $show_id= shift @_;
+	my $s_num =  shift @_;
+	my $e_num = shift @_;
+	
+	my $episodes;
+	
+	my $cache_key = $show_id;
+	
+	if (defined $tvr_cache->{episodes}->{$cache_key}) {
+		$episodes = $tvr_cache->{episodes}->{$cache_key};
+	} else {
+		my $result = WebService::TVRage::EpisodeListRequest->new('episodeID' => $show_id);
+		$episodes = $result->getEpisodeList();
+		$tvr_cache->{episodes}->{$cache_key} = $episodes;
+	}
+	
+	my $episode = $episodes->getEpisode($s_num, $e_num);
+
+	return $episode;
+} # tvrEpisodeSearch()
+
+sub tvrShowSearch {
+	my $show_name = shift @_;
+	my $cso;
+	
+	if (defined $tvr_cache->{show}->{$show_name}) {
+		$cso = $tvr_cache->{show}->{$show_name};
+	} else {
+		#logMsg "Searching for \"$show_name\"";
+		my $result = $shsrch->search($show_name);
+		if (defined $result) {
+			my @titles = $result->getTitleList();
+			my $cor_name = bestMatch($show_name, \@titles);
+			my $show = $result->getShow($cor_name);
+		
+			$tvr_cache->{show}->{$show_name} = $show;
+			$tvr_cache->{show}->{$cor_name} = $show;
+			
+			$cso = $show;
+		}
+	}
+	
+	return $cso;
+} # tvrShowSearch()
+
+sub inferContext {
+	my $file_path = shift @_;
+	my @dparts = split(/\//, $file_path);
+	my $filename = pop @dparts;
+	
+	my $ctxt;
+	
+	$ctxt->{category} = 'Other';
+	
+	if ($file_path =~ m/\/TV\//) {
+		$ctxt->{category} = 'TV';
+		$ctxt->{name} = $filename;
+		$ctxt->{episode_number} = 'X';
+		$ctxt->{season_number} = 'X';
+		$ctxt->{episode_title} = $filename;
+		$ctxt->{air_date} = 'MM/DD/YYYY';
+		
+		my $cd = pop @dparts;
+		my $pd = pop @dparts;
+		
+		if ($cd =~ m/^season\s+(\d+)$/gi) {
+			$ctxt->{name} = $pd;
+			$ctxt->{season_number} = $1;
+		} elsif ($cd =~ m/^(.+?)\s*?-[^-]+$/gi) {
+			$ctxt->{name} = $1;
+		} else {
+			$ctxt->{name} = $cd;
+		}
+		
+		if ($ctxt->{name}) {
+			if ($filename =~ m/(episode|part)\s+(\d+)/gi) {
+				#logMsg 'method 1';
+				$ctxt->{episode_number} = $2 + 0;
+			} elsif ($filename  =~ m/[sS]?(0?[1-9]|[12345][0-9])[xeE]?([0123][0-9])/gi) {
+				#logMsg 'method 2';
+				$ctxt->{season_number} = $1 + 0;
+				$ctxt->{episode_number} = $2 + 0;
+			} elsif ($filename =~ m/(\d?\d)(\d{2})/gi) {
+				#logMsg 'method 3';
+				$ctxt->{season_number} = $1 + 0;
+				$ctxt->{episode_number} = $2 + 0;
+			} elsif ($filename =~ m/^(\d{1,2})[^\d]/gi) {
+				$ctxt->{episode_number} = $1 + 0;
+			}
+			
+			my $show_obj = tvrShowSearch($ctxt->{name});#, 1);
+			
+			# show level
+			$ctxt->{url} = $show_obj->getLink() || '?';
+			$ctxt->{country} = $show_obj->getCountry() || '?';
+			$ctxt->{tv_rage_id} = $show_obj->getShowID() || '?';
+			$ctxt->{started} = $show_obj->getYearStarted() || '?';
+			$ctxt->{ended} = $show_obj->getYearEnded() || '?';
+			$ctxt->{status} = $show_obj->getStatus() || '?';
+			$ctxt->{genres} = $show_obj->getGenres();
+			
+			if ($ctxt->{season_number} =~ m/^\d+$/ && $ctxt->{season_number} =~ m/^\d+$/) {
+				my $ep_obj = tvrEpisodeSearch($show_obj->getShowID(), $ctxt->{season_number}, $ctxt->{episode_number});
+				if (defined $ep_obj) {
+					$ctxt->{episode_title} = $ep_obj->getTitle() || '?';
+					$ctxt->{air_date} = $ep_obj->getAirDate() || '?';
+				}
+			}
+		}
+	} else {
+		# Movie and Other
+	}
+	return $ctxt;
+} # inferContext()
+
+
+sub showUsage {
+	my $msg = shift @_;
+	
+	#logMsg "USAGE:";
+	#logMsg "bfl.pl <json-output-path> <thumb-path>";
+	
+	if (defined $msg) {
+		logMsg "  $msg";
+	}
+	
+	logMsg "";
+	exit 0;
+}
+
+my $video_dir = $config->{paths}->{video};
+my $root_dir = $config->{paths}->{root};
+my $thumb_width = $config->{thumbnails}->{width};
+my $thumb_count = $config->{thumbnails}->{count};
+my $chunks_per_file = int($config->{checksum}->{chunks_per_file});
+my $entropy = int($config->{checksum}->{entropy});
+
+if ($root_dir !~ m/\/$/) {
+	$root_dir .= '/';
+}
+
+
+my $json_output_to = $root_dir . $config->{paths}->{json};
+
+if (-d $json_output_to) {
+	if ($json_output_to !~ m/\/$/) {
+		$json_output_to .= '/';
+	}
+	$json_output_to .= 'media.json';
+}
+
+if (-f $json_output_to) {
+	unlink $json_output_to;
+}
+
+my $thumb_path = $root_dir . $config->{paths}->{thumbs};
+if (! -d $thumb_path) {
+	showUsage("Thumb path is not a valid directory.");
+}
+
+if ($thumb_path !~ m/\/$/) {
+	$thumb_path .= '/';
+}
+
+
+#my $video_dir = -d '/home/g33k/Documents/media/Video/Documentary/Dimensions (English)' ? '/home/g33k/Documents/media/Video/Documentary/Dimensions (English)' : '/Users/don/Desktop/Video';
+
+my @video_extensions = qw(avi mpg mpeg mp4 mov mkv);
+my @audio_extensions = qw(mp3 ogg m4a m4p wav aac);
+
+my $video_pattern = '\.(' . join('|', @video_extensions) . ')$';
+my $audio_pattern = '\.(' . join('|', @audio_extensions) . ')$';
+my $media_pattern = '\.(' . join('|', (@video_extensions, @audio_extensions)) . ')$';
+
+my $media_root;
+
+sub niceSize {
+	# Will work up to considerable file sizes!
+	
+	my $fs = shift @_ || 0; #$_[0];	# First variable is the size in bytes
+	my $dp = shift @_ || 0; #$_[1];	# Number of decimal places required
+	
+	my @units = ('bytes','kB','MB','GB','TB','PB','EB','ZB','YB');
+	my $u = 0;
+	$dp = ($dp > 0) ? 10**$dp : 1;
+	while($fs > 1024){
+		$fs /= 1024;
+		$u++;
+	}
+	if($units[$u]){ return (int($fs*$dp)/$dp)." ".$units[$u]; } else{ return int($fs); }
+}
+
+sub convert_seconds_to_hhmmss {
+  my $hourz=int($_[0]/3600);
+  my $leftover=$_[0] % 3600;
+  my $minz=int($leftover/60);
+  my $secz=int($leftover % 60);
+  return sprintf ("%02d:%02d:%02d", $hourz,$minz,$secz)
+}
+
+sub fileType {
+	my $file = shift @_;
+	
+	if ($file =~ m/$video_pattern/i) {
+		return 'video';
+	} elsif ($file =~ m/$audio_pattern/i) {
+		return 'audio';
+	} else {
+		return 'other';
+	}
+}
+
+sub loadDir {
+	my $dir = shift @_;
+	my $pattern = shift @_ || '.+';
+	my $md5 = Digest::MD5->new();
+	
+	if ($dir !~ /\/$/) {
+		$dir .= '/';
+	}
+	
+	opendir DFH, $dir or die "Can't read $dir: $!\n";
+	my @FILES = readdir DFH;
+	closedir DFH;
+	
+	my @ch_dirs;
+	my @ch_files;
+	
+	foreach my $file (@FILES) {
+		my $afp = $dir . $file;
+		if ($file !~ m/^\.{1,2}$/) {
+			if (-d $afp) {
+				push @ch_dirs, loadDir($afp, $pattern);
+			} else {
+				if ($file =~ m/$pattern/i) {
+					my $f_obj;
+					
+					
+					
+					$md5->reset();
+				
+					my ($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,$atime,$mtime,$ctime,$blksize,$blocks) = stat($afp);
+					my $eff_entropy = $entropy < $size ? $entropy : $size;
+					my $chunk_size = int($eff_entropy /  $chunks_per_file);
+					my $step_size = int($size / $chunks_per_file);
+					
+					my $CSFH;
+					open $CSFH, $afp || die "Can't read $afp: $!\n";
+					binmode($CSFH);
+					
+					#print "AFP: $afp\nSIZE: $size\nEFF ENTROPY: $eff_entropy\nSTEP SIZE: $step_size\nCHUNK SIZE: $chunk_size\nCHUNKS PER FILE: $chunks_per_file\n"; exit(0);
+					
+					for (my $offset = 0; $offset < $chunks_per_file - 1 ; $offset += $step_size) {
+						my $buf;
+						my $rc = read($CSFH, $buf, $chunk_size, $offset);
+						$md5->add($buf);
+						
+					}
+					close $CSFH;
+					
+					my $file_md5 = $md5->hexdigest();
+					
+					if (fileType($file) eq 'video') {
+						my $grabber = Video::FrameGrab->new(video => $afp);
+						
+						my $metadata = $grabber->meta_data();
+						
+						$f_obj->{meta} = $metadata;
+						$f_obj->{meta}->{duration} = convert_seconds_to_hhmmss($metadata->{length});		
+						
+						$f_obj->{meta}->{filename} =~ s/^$video_dir//gi;
+						
+						$f_obj->{ctxt} = inferContext($afp);
+						
+						logMsg $f_obj->{meta}->{filename};
+						if ($f_obj->{ctxt}->{category} eq 'TV') {
+							logMsg('Program: ' . $f_obj->{ctxt}->{name} . 
+							       ', Season: ' . $f_obj->{ctxt}->{season_number} .
+							       ', Episode: ' . $f_obj->{ctxt}->{episode_number} .
+							       ', Title: ' . $f_obj->{ctxt}->{episode_title});
+							       
+						}
+						#logMsg 's' . $f_obj->{ctxt}->{season_number} . 'e' . $f_obj->{ctxt}->{episode_number};
+						
+						$f_obj->{md5} = $file_md5;
+						
+						my @thumb_ar = ();
+						my $thumb_cnt = 0;
+						for my $p ($grabber->equidistant_snap_times($thumb_count)) {
+							$thumb_cnt++;
+							my $thumb_file = $file_md5 . '-' . $p . '.jpg';
+							my $img_fname = $thumb_path . $thumb_file;
+							
+							my $rel_img_fname = $img_fname;
+							$rel_img_fname =~ s/^$root_dir//gi;
+							
+							if (-e $img_fname) {
+								logMsg "    --> $rel_img_fname [exists]";
+								push @thumb_ar, $rel_img_fname;
+							} else {
+								my $frame = $grabber->snap($p);
+								my $image = GD::Image->new($frame);
+								if (defined $image) {
+									logMsg "    --> $rel_img_fname [created]";
+									
+									my ($width, $height) = $image->getBounds();
+									my $new_width = $thumb_width;
+									my $new_height = $height * ($new_width / $width);
+									$image = Image::Resize->new($image);
+									my $gd = $image->resize($new_width, $new_height);
+									$image = $gd->jpeg();
+									
+									
+									
+									push @thumb_ar, $rel_img_fname;
+									
+									open IFN, ">$img_fname" or die "Can't write image $img_fname: $!";
+									binmode IFN;
+									print IFN $image;
+									close IFN;
+								} else {
+									logMsg "    --> $rel_img_fname [failed]";
+								} # image created?
+							} # image exists?
+						} # each frame grab
+						$f_obj->{meta}->{thumbs} = \@thumb_ar;
+						
+						$f_obj->{size} = $size;
+						$f_obj->{created} = $ctime;
+						$f_obj->{modified} = $mtime;
+						$f_obj->{name} = $file;
+						$f_obj->{meta}->{niceSize} = niceSize($f_obj->{size}, 1);
+					
+						push @ch_files, $f_obj;
+						
+						#$media_root->{$f_obj->{meta}->{md5}} = $f_obj;
+						if ($f_obj->{ctxt}->{category} eq 'TV') {
+							my $sh_name = $f_obj->{ctxt}->{name};
+							my $sh_season = $f_obj->{ctxt}->{season_number};
+							my $sh_episode = $f_obj->{ctxt}->{episode_number};
+							
+							my $subep = '';
+							
+							while (defined $media_root->{TV}->{$sh_name}->{$sh_season}->{$sh_episode . $subep}) {
+								$subep = $subep eq '' ? 1 : $subep + 1;
+							}
+							
+							$media_root->{TV}->{$sh_name}->{$sh_season}->{$sh_episode . $subep} = $f_obj;
+						} elsif ($f_obj->{ctxt}->{category} eq 'Movie') {
+							#$media_root->{Movie}->{$f_obj->{ctxt}->{name}}->{$f_obj->{ctxt}->{season_number}}->{$f_obj->{ctxt}->{episode_number}} = $f_obj;
+						} else {
+							#$media_root->{Other}->{$f_obj->{ctxt}->{name}}->{$f_obj->{ctxt}->{season_number}}->{$f_obj->{ctxt}->{episode_number}} = $f_obj;
+						}
+						
+					} # video?
+					
+					
+				} # matches file pattern?
+			} # dir or file?
+		} # not . or ..
+	} #each file
+	
+	@ch_dirs = sort(@ch_dirs);
+	@ch_files = sort(@ch_files);
+	
+	my $dobj;
+	$dobj->{name} = basename($dir);
+	$dobj->{dirs} = \@ch_dirs;
+	$dobj->{files} = \@ch_files;
+	
+	return $dobj;
+}
+
+
+my $root = loadDir($video_dir, $media_pattern);
+
+my $base_obj;
+
+$base_obj->{media} = $media_root;
+$base_obj->{provider} = $config->{provider};
+
+open MFH, ">$json_output_to" or die "Can't write JSON to $json_output_to: $!\n";
+#print MFH encode_json($root);
+print MFH encode_json($base_obj);
+close MFH;
+
+logMsg;
+logMsg "done.";
---
+++ js/medac.js
@@ -0,0 +1,159 @@
+$(function() {
+	var tmplDir = $('#tmplDirEntry').html();
+	var tmplVid = $('#tmplVideoEntry').html();
+	var tmplThumbs = $('#tmplThumbs').html();
+	var tmplNode = $('#tmplNodeEntry').html();
+	
+	var formatFileSize = function(size) {
+		
+	}; // formatFileSize()
+	
+	var renderFile = function($attach_to, file_node) {
+		//console.log("FILE: " + file_node.name);
+		if (typeof file_node.meta !== 'undefined' && typeof file_node.meta.length !== 'undefined' && file_node.meta.length > 5 * 60) {
+			var $file = $(Mustache.render(tmplVid, file_node));
+			$file.find('td.thumb').html(Mustache.render(tmplThumbs, file_node));
+			$attach_to.append($file);
+		}
+	}; // renderFile();
+	
+	//var renderDir = function($attach_to, dir_node) {
+	//	//console.log('DIR: ' + dir_node.name);
+	//	var $dir = $(Mustache.render(tmplDir, dir_node));
+	//	$attach_to.append($dir);
+	//	
+	//	var $children = $dir.find('ul.children');
+	//	
+	//	if (typeof dir_node.dirs !== 'undefined') {
+	//		for (var i = 0; i < dir_node.dirs.length; i++) {
+	//			var dir = dir_node.dirs[i];
+	//			renderDir($children, dir);
+	//		}
+	//	}
+	//	
+	//	if (typeof dir_node.files !== 'undefined') {
+	//		for (var i = 0; i < dir_node.files.length; i++) {
+	//			var file = dir_node.files[i];
+	//			renderFile($children, file);
+	//		}
+	//	}
+	//}; // renderDir();
+	
+	var renderEpisode = function($attach_to, episodes, episode_number) {
+		var episode = episodes[episode_number];
+		
+		renderFile($attach_to, episode);
+	}; // renderEpisode()
+	
+	var renderSeason = function($attach_to, seasons, season_number) {
+		var season = seasons[season_number];
+		var $season = $(Mustache.render(tmplNode, 'Season ' + season_number));
+		$attach_to.append($season);
+		var $children = $season.find('ul.children');
+		for (var episode_number in season) {
+			if (season.hasOwnProperty(episode_number)) {
+				renderEpisode($children, season, episode_number);
+			}
+		}
+	}; // renderSeason)
+	
+	var renderShow = function($attach_to, shows, show_name) {
+		var show = shows[show_name];
+		var $show = $(Mustache.render(tmplNode, show_name));
+		$attach_to.append($show);
+		var $children = $show.find('ul.children');
+		for (var season_number in show) {
+			if (show.hasOwnProperty(season_number)) {
+				renderSeason($children, show, season_number);
+			}
+		}
+	}; // renderShow()
+	
+	var renderTV = function($attach_to, node) {
+		var $tv = $(Mustache.render(tmplNode, 'TV'));
+		
+		$attach_to.append($tv);
+		var $children = $tv.find('ul.children');
+		
+		for (var show_name in node) {
+			if (node.hasOwnProperty(show_name)) {
+				renderShow($children, node, show_name);
+				
+				//var show = node[show_name];
+				//var $show = $(Mustache.render(tmplNode, show_name));
+				//$attach_to.append($show);
+				//var $children = $show.find('ul.children');
+				//for (var season_number in show) {
+				//	if (show.hasOwnProperty(season_number)) {
+				//		
+				//	}
+				//}
+			}
+		}
+	}; // renderTV()
+	
+	$.getJSON('media/media.json', {}, function(data, status, xhr) {
+		
+		var $root = $('#root');
+		
+		console.log(data);
+		
+		var media = data.media;
+		
+		//renderDir($root, media);
+		
+		renderTV($root, media.TV);
+		
+		$('.dirLink').click(function(e) {
+			$ctxt = $(this);
+			$parent = $ctxt.parent('li');
+			
+			if ($parent.hasClass('closed')) {
+				var eo = typeof $parent.data('opened') !== 'undefined';
+				if (eo) {
+					$parent.children('td.thumbs');
+				} else {
+					$parent.data('opened','true');
+				}
+				
+				$parent.children('ul').slideDown(250);
+				$parent.removeClass('closed');
+			} else {
+				$parent.children('ul').slideUp(250);
+				$parent.addClass('closed')
+			}
+			
+			//$parent.toggleClass('closed');
+			
+			if (e.preventDefault) { e.preventDefault(); }
+						
+			return false;
+		});
+		
+		$('.showThumbs').click(function(e) {
+			var $a = $(this);
+			$a.fadeOut(250, function() { $(this).remove(); });
+			var $div = $a.parent('.hiddenThumbs');
+			var $imgs = $div.find('img.thumb');
+			
+			var cnt = 0;
+			var fade_time = Math.floor(1000 / $imgs.length);
+			
+			$imgs.each(function(idx, elem) {
+				var $img = $(elem);
+				
+				$img.attr('src', $img.data('src'));
+				setTimeout(function() {
+					$img.fadeIn(fade_time);
+				}, Math.floor(cnt * fade_time * 0.5));
+				cnt++;
+				
+			});
+			
+			//$div.removeClass('hiddenThumbs');
+			$a.remove();
+			
+			e.preventDefault();
+		});
+	});
+});
