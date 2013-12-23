e1c6b811ef9b45f2758af5e15aec15200b62b2ba
g33k <g33k@medac.(none)>
2013-12-23 07:50:22 -0600

    rsync progress monitor

--- medac/progrsync.pl
+++ medac/progrsync.pl
@@ -6,6 +6,7 @@ use Getopt::Long;
 use Cache::FileCache;
 use Number::Bytes::Human qw(format_bytes);
 use Time::HiRes qw(gettimeofday usleep);
+use DateTime::Format::Duration;
 
 my $src_d = 0;
 my $dest_d = 0;
@@ -15,11 +16,13 @@ my $poll = 1;
 GetOptions(
 	's|src=s' => \$src_d,
 	'd|dest=s' => \$dest_d,
-	'delay|ms|milliseconds=i' => \$delay,
+	'delay|secondss=f' => \$delay,
 	'poll!' => \$poll
 );
 
-$delay *= 1000;
+# I need a 111 ms delay on my system.  autotune this at some point.
+$delay = (($delay < .111 ? .112 : $delay) - 0.111) * 1000000;
+
 
 my $cache = Cache::FileCache->new();
 
@@ -44,27 +47,63 @@ sub parse {
 
 if (-d $src_d && -d $dest_d) {
 	my $human = Number::Bytes::Human->new(bs => 1000, si => 1);
+	my $d = DateTime::Format::Duration->new(
+		pattern => '%d:%H:%M:%S',
+		normalize => 1
+	);
+	
+	my $last_src;
+	my $last_dest;
+		
 	
 	while (1) {
-		my $last_src = $cache->get($src_d);
-		my $last_dest = $cache->get($dest_d);
+		
+		
 	
 		my $src = parse(`du -s -b $src_d 2>/dev/null`);
 		my $dest = parse(`du -s -b $dest_d 2>/dev/null`);
 		my $percent = sprintf('%.3f', ($dest->{bytes} / $src->{bytes}) * 100);
 		
+		$last_src = !$last_src ? $cache->get($src_d) : $last_src; 
+		$last_dest = !$last_dest ? $cache->get($dest_d) : $last_dest;
+		
 		print "\n";
 		print $human->format($dest->{bytes}) . ' of ' . $human->format($src->{bytes}) . " ($percent\%) - " . $human->format($src->{bytes} - $dest->{bytes}) . " remaining\n";
 		if ($last_dest) {
 			my $bytes_xfered = $dest->{bytes} - $last_dest->{bytes};
-			my $timespan = $dest->{ts} - $last_dest->{ts};
+			my $timespan = ($dest->{ts} - $last_dest->{ts}) || 1;
+			#my $time_diff = abs(($timespan * 1000000) - $delay);
+			#print '' . ($timespan * 1000000) . ' <> ' . $delay . ' : ' . $time_diff;
+			#if ($timespan * 1000000 > $delay) {
+			#	print '-';
+			#} elsif ($timespan * 1000000 < $delay) {
+			#	print '+';
+			#}
+			#print "\n";
+			
 			my $bps = $bytes_xfered / $timespan;
+			my $etr = ($src->{bytes} - $dest->{bytes}) / $bps;
+			
+			my $fmt_etr = $d->format_duration(
+				DateTime::Duration->new(
+					seconds => $etr
+				)
+			);
+			
 			print $human->format($bytes_xfered) . ' in ' . sprintf('%.2f', $timespan) . " seconds \@ " . $human->format($bps) . " / second\n";
+			print "$fmt_etr remaining\n";
+			
+			
 		}
 		
+		$last_src = $src;
+		$last_dest = $dest;
 		
-		$cache->set($src_d, $src);
-		$cache->set($dest_d, $dest);
+		if (!$poll) {
+			$cache->set($src_d, $src); 
+			$cache->set($dest_d, $dest);
+			exit(0);
+		}
 		usleep($delay);
 	}
 }
