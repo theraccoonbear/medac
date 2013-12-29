50cb7dc9f5d26ba7203daf1d42f6fabc96976708
Don Smith <don.c.smith@gmail.com>
2013-12-29 17:46:40 -0600

    HM support

--- medac/scripts/plex.pl
+++ medac/scripts/plex.pl
@@ -1,4 +1,8 @@
 #!/usr/bin/perl
+BEGIN {
+    my $b__dir = (-d '/home2/theracco/perl'?'/home2/theracco/perl':( getpwuid($>) )[7].'/perl');
+    unshift @INC,$b__dir.'5/lib/perl5',$b__dir.'5/lib/perl5/x86_64-linux-thread-multi',map { $b__dir . $_ } @INC;
+}
 use strict;
 use warnings;
 
