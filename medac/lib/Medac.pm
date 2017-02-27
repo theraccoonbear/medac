d338465dacf3b07af3b52f7a9df0df3d60330173
Don Smith <don.c.smith@gmail.com>
2017-02-26 19:51:00 -0600

    baaa

--- medac/lib/Medac.pm
+++ medac/lib/Medac.pm
@@ -12,10 +12,10 @@ use JSON::XS;
 use File::Slurp;
 use Data::Dumper;
 #use Slurp;
-use CGI;
+#use CGI;
 use POSIX;
 use Medac::API::Default;
-use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
+#use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
 use Cwd qw(abs_path cwd);
 use Moose::Util::TypeConstraints;
 
