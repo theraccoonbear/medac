198305a1d031c9682faee6a2dd34c43d36d40971
Don Smith <don.c.smith@gmail.com>
2017-03-07 17:32:20 -0600

    fixing up WRS

--- medac/scripts/the-wrs.pl
+++ medac/scripts/the-wrs.pl
@@ -5,19 +5,19 @@ use lib 'lib';
 use Data::Printer;
 use Cwd 'abs_path';
 use File::Basename;
-use lib dirname(abs_path($0)) . '/..';
+use lib dirname(abs_path($0)) . '/../lib/';
 use Medac::Downloader::WRS;
 
 my $wrs = new Medac::Downloader::WRS();
 
-#my $episodes = $wrs->listEpisodesForSeason(2015, 2016);
-#p($episodes);
+my $episodes = $wrs->listEpisodesForSeason(2009, 2010);
+p($episodes);
 
 #$wrs->grabEpisode(2365554475, '../media/The Woodwright\'s Shop - s35e08 - Bowl Carving with Peter Follansbee.mp4');
 #$wrs->grabEpisode(2365554505, '../media/The Woodwright\'s Shop - s35e09 - Hollows and Rounds.mp4');
-$wrs->grabEpisode(2365554510, '../media/The Woodwright\'s Shop - s35e10 - Welsh Stick Chair I.mp4');
-$wrs->grabEpisode(2365554518, '../media/The Woodwright\'s Shop - s35e11 - Welsh Stick Chair II.mp4');
-$wrs->grabEpisode(2365554522, '../media/The Woodwright\'s Shop - s35e12 - Tool Smithing with Peter Ross.mp4');
+#$wrs->grabEpisode(2365554510, '../media/The Woodwright\'s Shop - s35e10 - Welsh Stick Chair I.mp4');
+#$wrs->grabEpisode(2365554518, '../media/The Woodwright\'s Shop - s35e11 - Welsh Stick Chair II.mp4');
+#$wrs->grabEpisode(2365554522, '../media/The Woodwright\'s Shop - s35e12 - Tool Smithing with Peter Ross.mp4');
 
 
 #foreach my $ep (@$episodes) {
